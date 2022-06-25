5d0b418db02ccd6511a27585f4576dfe50a910a3
Leandro Ricardo <leandroricardo@live.com>
2022-06-25 18:31:24 -0300

    SellerFormViewModel and Department select component

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -43,6 +43,8 @@ namespace SalesWebMvc
 
             services.AddScoped<SeedingService>();
             services.AddScoped<SellerService>();
+            services.AddScoped<DepartmentService>();
+
         }
 
 
